fa0fe4b1701a9155d60adb4d3ee37a07fec05d98
zhangpanlong <zhangpanlong@jiuqi.com.cn>
2020-09-16 10:25:45 +0800

    third commit

--- src/GoodsSalePlatform/src/main/java/com/goods/manage/impl/Customer.java
+++ src/GoodsSalePlatform/src/main/java/com/goods/manage/impl/Customer.java
@@ -17,6 +17,17 @@ public class Customer {
 	 * 顾客地址
 	 */
 	private String customerAddress;
+	/**
+	 * 顾客属性（新老顾客）
+	 */
+	private String customerAttr;
+	
+	public String getCustomerAttr() {
+		return customerAttr;
+	}
+	public void setCustomerAttr(String customerAttr) {
+		this.customerAttr = customerAttr;
+	}
 	public String getCustomerName() {
 		return customerName;
 	}
