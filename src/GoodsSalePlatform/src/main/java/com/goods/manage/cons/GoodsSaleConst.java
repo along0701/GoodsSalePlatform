e0b6668b48238d2f7459a323d6f5414aa2f824cd
zhangpanlong <zhangpanlong@jiuqi.com.cn>
2020-09-16 17:35:28 +0800

    stage commit test

---
+++ src/GoodsSalePlatform/src/main/java/com/goods/manage/cons/GoodsSaleConst.java
@@ -0,0 +1,15 @@
+package com.goods.manage.cons;
+
+public class GoodsSaleConst {
+	public final static String PERIOD = "PERIOD";
+	
+	public final static String WEEK = "ÖÜ";
+	
+	public final static String MONTH = "ÔÂ";
+	
+	public final static String YEAR = "Äê";
+	
+	public final static String SEASON = "¼¾";
+	
+	public final static String CREDITSCORE = "CREDITSCORE";
+}
---
+++ src/GoodsSalePlatform/src/main/java/com/goods/manage/cons/SalePersonConst.java
@@ -0,0 +1,18 @@
+package com.goods.manage.cons;
+
+public class SalePersonConst {
+	/**
+	 * IAM平台用户同步参数： URL
+	 */
+	public static final String URL = "url";
+	
+	/**
+	 * IAM平台用户同步参数：INTEGRATIONKEY
+	 */
+	public static final String INTEGRATIONKEY = "integrationKey";
+	
+	/**
+	 * IAM平台用户同步参数：SYSTEMCODE
+	 */
+	public static final String SYSTEMCODE = "systemCode";
+}
