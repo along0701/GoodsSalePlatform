60f7ac7b6d780f4a203a231c069700a2dcbe688d
zhangpanlong <zhangpanlong@jiuqi.com.cn>
2020-09-17 11:25:03 +0800

    dev_along commit

---
+++ src/GoodsSalePlatform/src/main/java/com/goods/manage/impl/LifeGoods.java
@@ -0,0 +1,59 @@
+package com.goods.manage.impl;
+/**
+ * 顾客实体类
+ * @author zhangpanlong
+ *
+ */
+public class LifeGoods extends Goods{
+	/**
+	 * 顾客名称
+	 */
+	private String customerName;
+	/**
+	 * 顾客编码
+	 */
+	private String customerNo;
+	/**
+	 * 顾客地址
+	 */
+	private String customerFictAddress;
+	/**
+	 * 顾客属性（新老顾客）
+	 */
+	private String customerAttr;
+	/**
+	 * 年购买额
+	 */
+	private double buyQuto;
+	
+	public double getBuyQuto() {
+		return buyQuto;
+	}
+	public void setBuyQuto(double buyQuto) {
+		this.buyQuto = buyQuto;
+	}
+	public String getCustomerAttr() {
+		return customerAttr;
+	}
+	public void setCustomerAttr(String customerAttr) {
+		this.customerAttr = customerAttr;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getCustomerNo() {
+		return customerNo;
+	}
+	public void setCustomerNo(String customerNo) {
+		this.customerNo = customerNo;
+	}
+	public String getCustomerFictAddress() {
+		return customerFictAddress;
+	}
+	public void setCustomerFictAddress(String customerFictAddress) {
+		this.customerFictAddress = customerFictAddress;
+	}
+}
